2c71729937549b2a30e1b102fbb987dd1b4ea3d4
Andriikryl <andreyk557@gmail.com>
2023-04-01 22:55:06 +0300

    feat[draw-func]

--- js/app.js
+++ js/app.js
@@ -1,15 +1,39 @@
 () => {
+  const config = {
+    dotMinRad: 6,
+    dotMaxRad: 20,
+    massFactor: 0.002,
+    defColor: `rgba(250, 10, 30, 0.9)`,
+  };
+
+  const TWO_PI = 2 * Math.PI;
+
   const canvas = document.querySelector("canvas");
   const ctx = canvas.getContext("2d");
 
-  let w, h, mouse;
+  let w, h, mouse, dots;
 
   class Dot {
     constructor() {
       this.pos = { x: mouse.x, y: mouse.y };
       this.vel = { x: 0, y: 0 };
-      this.rad = random(6, 20);
+      this.rad = random(config.dotMinRad, config.dotMaxRad);
+      this.mass = this.rad * config.massFactor;
+      this.color = config.defColor;
     }
+
+    draw() {
+      creatCircle(this.pos.x, this.pos.y, this.rad, true, this.color);
+      creatCircle(this.pos.x, this.pos.y, this.rad, false, config.defColor);
+    }
+  }
+
+  function creatCircle(x, y, rad, fill, color) {
+    ctx.fillStyle = ctx.strokeStylec = color;
+    ctx.beginPath();
+    ctx.arc(x, y, rad, 0, TWO_PI);
+    ctx.closePath();
+    fill ? ctx.fill() : ctx.stroke();
   }
 
   function random(min, max) {
@@ -21,6 +45,16 @@
     h = canvas.height = innerHeight;
 
     mouse = { x: w / 2, y: h / 2, down: false };
+    dots = [];
+  }
+
+  function loop() {
+    ctx.clearRect(0, 0, w, h);
+
+    if (mouse.down) {
+      dots.push(new Dot());
+    }
+    dots.map((e) => e.draw());
   }
 
   init();
